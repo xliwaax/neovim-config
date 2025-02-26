dd762834ec868c5849c2aaa85ac6ecc2dcb28403
liwaa hajyahya <liwaah@mobileye.com>
2025-02-26 16:43:00 +0200

    fit: typo

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -5,8 +5,7 @@ require("bufferline").setup({
     separator_style = "slant",  -- Style of separators
     max_name_length = 18,  -- Maximum buffer name length
     max_prefix_length = 15,  -- Maximum length of buffer prefix
-    enforce_regular_tabs = true,  -- Make all tabs have the same width
-    tab_size = 18,  -- Width of each tab
+    enforce_regular_tabs = false,  -- Make all tabs have the same width
   },
 })
 
